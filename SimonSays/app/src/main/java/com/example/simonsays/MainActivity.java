4cb1e368fe20e9d19e123036af74e2f67f79e83d
magicznyprzemek <bprzywara11@gmail.com>
2022-03-11 23:46:12 +0100

    Revert "basic layout"
    This reverts commit 77fafb842420101a325efcd901574d79e24474d9.

--- SimonSays/app/src/main/java/com/example/simonsays/MainActivity.java
+++ SimonSays/app/src/main/java/com/example/simonsays/MainActivity.java
@@ -1,34 +1,14 @@
 package com.example.simonsays;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.Toast;
-import android.view.View;
 
-import java.util.Random;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button startButton;
-    public Button redButton;
-    public Button blueButton;
-    public Button greenButton;
-    public Button yellowButton;
-    Random rand = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startButton= (Button) findViewById(R.id.buttonStart);
-        redButton= (Button) findViewById(R.id.redButton);
-        blueButton= (Button) findViewById(R.id.blueButton);
-        greenButton= (Button) findViewById(R.id.greenButton);
-        yellowButton= (Button) findViewById(R.id.yellowButton);
-    }
-
-    public void StartGame(View v)
-    {
-
     }
 }
