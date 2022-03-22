487e2fdb8b63c35394107d4c651b635151d96e2d
magicznyprzemek <bprzywara11@gmail.com>
2022-03-22 10:27:12 +0100

    UI

--- sudoku10/app/src/main/java/com/example/sudoku10/MainActivity.java
+++ sudoku10/app/src/main/java/com/example/sudoku10/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.sudoku10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
+    TextView[][] TextArray=new TextView[9][9];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ConnectArray();
     }
+    private void ConnectArray()
+    {
+
+        int z=0;
+        for(int i=0; i<9; i++)
+        {
+            for(int j=0; j<9; j++)
+            {
+                int id = this.getResources().getIdentifier("et"+z, "id", this.getPackageName());
+                System.out.println(id);
+                try {
+                    TextArray[i][i]=(TextView) findViewById(id);
+                    TextArray[i][i].setText("1");
+                    TextArray[i][i].setInputType(InputType.TYPE_CLASS_NUMBER);
+                    TextArray[i][i].setFilters(new InputFilter[] {new InputFilter.LengthFilter(1)});
+
+
+                }
+                catch (Exception x) { } // xd
+                if(z<81)
+                    z++;
+            }
+        }
+    }
+
+
 }
---
+++ sudoku10/app/src/main/java/com/example/sudoku10/SudokuGenerator.java
@@ -0,0 +1,6 @@
+package com.example.sudoku10;
+
+public class SudokuGenerator {
+    public int[][] SudokuArray= new int[9][9];
+
+}
