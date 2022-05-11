e17ac08564a3611f4160201b769fad7b9d8cca97
magicznyprzemek <bprzywara11@gmail.com>
2022-05-11 22:33:06 +0200

    fixed everything

--- SoundBoard/app/src/main/java/com/example/soundboard/MainActivity.kt
+++ SoundBoard/app/src/main/java/com/example/soundboard/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.soundboard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Color
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
-import android.content.Context
-import android.media.MediaPlayer
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.io.*
-import java.lang.Exception
 
-class MainActivity : AppCompatActivity()
-{
+
+class MainActivity : AppCompatActivity() {
     var Z = R.raw.sound1
     var A = R.raw.sounda
     var B = R.raw.soundb
@@ -21,16 +21,37 @@ class MainActivity : AppCompatActivity()
 
     var btnArray: Array<Button?> = Array(6) { null }
     var saveList: MutableList<Int> = ArrayList()
-    var mp : MediaPlayer = MediaPlayer()
-
+    var mp: MediaPlayer = MediaPlayer()
+    var FileName = "dataFile0.txt"
+    var file1 = File(FileName)
+    var isRecording: Boolean = false
+  //  val recordButton: Button? = null
+  //  var playbButton: Button? = null
+  //  var sekwencjaButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         arrayConnect(btnArray)
-        write("ala")
-        write("ma")
-        write("kota")
+        val recordButton: Button? = findViewById(R.id.RecordBtn)
+        val playButton: Button? = findViewById(R.id.button14)
+       // val sekwencjaButton: Button? = findViewById(R.id.history)
+        playButton?.setBackgroundColor(Color.BLACK)
+        recordButton?.setBackgroundColor(Color.BLACK)
+    }
+
+    fun clearFile() // inaczej się nie udawało
+    {
+        try {
+
+            val FileOut: FileOutputStream = openFileOutput(file1.path, MODE_PRIVATE)
+            val outWriter = OutputStreamWriter(FileOut)
+            outWriter.write("")
+            outWriter.close()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
 
 
     }
@@ -44,53 +65,102 @@ class MainActivity : AppCompatActivity()
     }
 
     fun playSound(context: Context, SoundID: Int) {
-       // mp.start()
-        mp=MediaPlayer.create(context, SoundID)
+        mp = MediaPlayer.create(context, SoundID)
         mp.start()
     }
 
     fun showIds(v: View) {
-        for (i in 0 until saveList.size) {
-            Toast.makeText(this, saveList[i].toString(), Toast.LENGTH_SHORT).show()
+        if(!saveList.isEmpty()) {
+            var line = ""
+            for (i in 0 until saveList.size) {
+                line = line + translateToLetter(saveList[i]) + "-"
+            }
+            Toast.makeText(this, line, Toast.LENGTH_SHORT).show()
         }
+        else
+            Toast.makeText(this, "BRAK SEKWENCJI", Toast.LENGTH_SHORT).show()
+
     }
-    fun read(v : View)
-    {
-        try
-        {
-            val FileIN : FileInputStream = openFileInput("dataFile.txt")
-            val InputRead : InputStreamReader = InputStreamReader(FileIN)
-            var text: String = ""
-            while(!text.isEmpty())
-            {
-                text = InputRead.readText()
-                Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+    fun read(v: View) {
+        try {
+            val FileIN: FileInputStream = openFileInput(file1.path)
+            val InputRead = InputStreamReader(FileIN)
+            val linie = InputRead.readLines()
+            InputRead.close()
+            if (!linie.isEmpty()) {
+                val splitLine = linie[0].split(';')
+                for (i in 0 until splitLine.size) {
+                    if (splitLine[i] != "") {
+                        playSound(this, splitLine[i].toInt())
+                        Thread.sleep(700)
+                    }
+                }
+            }
+            else {
+                Toast.makeText(this, "BRAK SEKWENCJI", Toast.LENGTH_SHORT).show()
             }
 
-        }
-        catch (e: Exception)
-        {
+        } catch (e: IOException) {
             throw e
         }
 
     }
-    fun write(line :String)
-    {
-      try
-      {
-         var fileOut :FileOutputStream = openFileOutput("dataFile.txt", MODE_PRIVATE)
-          val out_writer: OutputStreamWriter = OutputStreamWriter(fileOut)
-          out_writer.write(line)
-          out_writer.close()
 
-      }
-      catch (e: Exception)
-      {
-          e.printStackTrace()
-      }
+    fun write() {
+        var line = ""
+        try {
+
+            val FileOut: FileOutputStream = openFileOutput(file1.path, MODE_APPEND)
+            val outWriter = OutputStreamWriter(FileOut)
+            for (i in 0 until saveList.size)
+                line += saveList[i].toString() + ";"
+            outWriter.write(line)
+            outWriter.close()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun starRecord(v: View) {
+        if (isRecording) {
+            clearFile()
+            write()
+            isRecording = false
+            v.setBackgroundColor(Color.BLACK)
+
+        } else if (!isRecording) {
+            saveList.clear()
+            isRecording = true
+            v.setBackgroundColor(Color.RED)
+
+        }
+
+    }
+
+    fun translateToLetter(x: Int): Char {
+        val s: Char = when {
+            x == A -> 'A'
+            x == B -> 'B'
+            x == C -> 'C'
+            x == D -> 'D'
+            x == E -> 'E'
+            x == Z -> 'Z'
+            else -> '-'
+        }
+        return s
+    }
+
+    fun setREd() {
+        for (i in 0 until btnArray.size) {
+            btnArray[i]?.setBackgroundColor(Color.RED)
+        }
     }
 
     fun soundButtons(v: View) {
+        setREd()
+        v.setBackgroundColor(Color.GREEN)
         val s: Int = when (v.id) {
             btnArray[0]?.id -> A
             btnArray[1]?.id -> B
@@ -101,6 +171,9 @@ class MainActivity : AppCompatActivity()
             else -> 0 // xd
         }
         playSound(this, s)
-        saveList.add(s)
+        if (isRecording) {
+            saveList.add(s)
+        }
+
     }
 }
